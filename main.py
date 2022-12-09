6e69d9846da3668c7300354ab23864149f829646
procrastimax <mmeheykeroth@protonmail.com>
2022-12-09 15:38:35 +0100

    Implements basic training of resnet with imagenette

---
+++ main.py
@@ -0,0 +1,69 @@
+from utils import *
+from keras import activations
+import tensorflow as tf
+
+
+# to make effectrs of the change we need to apply the modifcations of the model
+# like i.e. here: https://github.com/raghakot/keras-vis/blob/master/vis/utils/utils.py
+# for this we need to save and reload the model
+# TODO: check if this is still currently needed
+
+
+RUN_NAME = "resnet-imagenette-relu-1"
+
+
+def main():
+    AUTOTUNE = tf.data.AUTOTUNE
+
+    BATCH_SIZE = 24
+    train_ds = load_train_ds("imagenette/160px-v2", batch_size=BATCH_SIZE)
+    val_ds = load_val_ds("imagenette/160px-v2", batch_size=BATCH_SIZE)
+
+    # resize datasets
+    SIZE = (224, 224)
+    train_ds = train_ds.map(lambda x, y: (resize_image(x, SIZE), y), num_parallel_calls=AUTOTUNE)
+    val_ds = val_ds.map(lambda x, y: (resize_image(x, SIZE), y), num_parallel_calls=AUTOTUNE)
+
+    # preprocess dataset
+    train_ds = train_ds.map(lambda x, y: (tf.keras.applications.resnet50.preprocess_input(x), y), num_parallel_calls=AUTOTUNE)
+    val_ds = val_ds.map(lambda x, y: (tf.keras.applications.resnet50.preprocess_input(x), y), num_parallel_calls=AUTOTUNE)
+
+    train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+    val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+    model: Model = ResNet50(
+        include_top=True,
+        weights=None,
+        input_tensor=None,
+        input_shape=None,
+        pooling=None,
+        classes=10,
+    )
+
+    #change_activation_function(model, activations.tanh)
+
+    model.compile(
+        optimizer="adam",
+        loss="sparse_categorical_crossentropy",
+        metrics=["accuracy"])
+
+    # checkpoint setup
+    checkpoint_path = f"training/{RUN_NAME}/cp.ckpt"
+
+    cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path, save_weights_only=True, verbose=1)
+
+    epochs = 10
+    history = model.fit(
+        train_ds,
+        validation_data=val_ds,
+        epochs=epochs,
+        callbacks=[cp_callback]
+    )
+
+    model.save(f"saved_model/{RUN_NAME}.h5")
+
+    visualize_training(history, epochs)
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,86 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from keras import Model, activations
+from keras.applications import ResNet50
+from keras.layers.core.activation import Activation
+import tensorflow_datasets as tfds
+
+
+def load_train_ds(model_name: str, batch_size: int = 32):
+    # use imagenette model for testing purposes
+    ds = tfds.load(
+        model_name,
+        split=["train"],
+        as_supervised=True,
+        batch_size=batch_size,
+        shuffle_files=True,
+        with_info=False
+    )
+    return ds[0]
+
+
+def load_val_ds(model_name: str, batch_size: int = 32):
+    # use imagenette model for testing purposes
+    ds = tfds.load(
+        model_name,
+        split=["validation"],
+        as_supervised=True,
+        batch_size=batch_size,
+        with_info=False
+    )
+    return ds[0]
+
+
+def resize_image(images, size):
+    """
+        Resizes given images to size
+    """
+    return tf.image.resize(images, size)
+
+
+def change_activation_function(model: Model, new_activation_function):
+    """
+        Sets new activation function to all 'Activation' layer in a model
+        This function works in-place and does not return anything.
+
+        new_activation_function needs to be one of keras.activations
+    """
+
+    # TODO: do we want to change all activation function (Conv2D too) or just from the activation layer?
+#    for layer in model.layers:
+#        # check if layer is an activation layer - if so, then replace activation function
+#        if isinstance(layer, Activation):
+#            layer.activation = new_activation_function
+
+    # is this line needed???
+    model.activation = activations.tanh
+
+    for layer in model.layers:
+        # check if layer is an activation layer - if so, then replace activation function
+        if hasattr(layer, 'activation'):
+            layer.activation = new_activation_function
+
+
+def visualize_training(history, epochs):
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs_range = range(epochs)
+
+    plt.figure(figsize=(8, 8))
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs_range, acc, label='Training Accuracy')
+    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs_range, loss, label='Training Loss')
+    plt.plot(epochs_range, val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+    plt.show()
+
